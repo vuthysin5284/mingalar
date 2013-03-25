c8be7b7fe25e6b1d66467bd4c4784cb53cb5965f
vuthy <vuthysin5284@yahoo.com>
2013-03-25 19:14:04 +0700

    not have change

--- utilities/system.conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	define("record",100);
-	define("page",10);
-	
-	$config["sex"] = array("Female" => "Female" , "Male"=>"Male");
-	$config["result"] = array("Fial" => "Fial" , "Pass"=>"Pass");
-
-?>
